6849447acc8802461e1591f5bff479128af5e7a3
ngkiddle <ngkiddle@gmail.com>
2020-03-12 17:03:15 -0700

    Init navbar

--- app/src/main/java/com/example/android/sqliteweather/data/AppDatabase.java
+++ app/src/main/java/com/example/android/sqliteweather/data/AppDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {ForecastLocation.class}, version = 1)
+@Database(entities = {ForecastLocation.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract SavedCityDao savedCitiesDao();
     private static volatile AppDatabase INSTANCE;
