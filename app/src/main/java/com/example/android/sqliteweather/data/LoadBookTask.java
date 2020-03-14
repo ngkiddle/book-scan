36821cbff4c93e669119c1e2aed31cf395bbb601
Halston <sellenth@oregonstate.edu>
2020-03-14 10:54:04 -0700

    deleted files

--- app/src/main/java/com/example/android/sqliteweather/data/LoadBookTask.java
+++ app/src/main/java/com/example/android/sqliteweather/data/LoadBookTask.java
@@ -7,14 +7,7 @@ import com.example.android.sqliteweather.utils.GoogleBooksUtils;
 
 import java.io.IOException;
 
-/*
- * This is our AsyncTask for loading forecast data from OWM.  It mirrors the AsyncTask we used
- * previously for loading the forecast, except here, we specify an interface AsyncCallback to
- * provide the functionality to be performed in the main thread when the task is finished.
- * This is needed because, to avoid memory leaks, the AsyncTask class is no longer an inner class,
- * so it can no longer directly access the fields it needs to update when loading is finished.
- * Instead, we provide a callback function (using AsyncCallback) to perform those updates.
- */
+
 public class LoadBookTask extends AsyncTask<Void, Void, String> {
 
     public interface AsyncCallback {
